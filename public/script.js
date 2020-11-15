1b4851f55115e832c25915878eddc584c8bc0649
Venika Gaur <Venika.gaur2610@gmail.com>
2020-11-15 04:39:59 -0600

    Final updates

--- public/script.js
+++ public/script.js
@@ -1,13 +1,38 @@
 const WHITE=['a','s','d','f','g','h','j','k','z','x','c','v','b','n']
 const BLACK=['q','w','e','r','t','y','u','i','o','p']
 
+const recordButton = document.querySelector('.recordBtn')
+const playButton = document.querySelector('.playBtn')
+const saveButton = document.querySelector('.saveBtn')
+const songLink = document.querySelector('.songLink')
 const keys = document.querySelectorAll('.key')
 const whiteKeys = document.querySelectorAll('.key.white')
 const blackKeys = document.querySelectorAll('.key.black')
 
+let recordingStartTime
+let songNotes = currentSong && currentSong.notes
+
+//Will return key for the notes
+const keyMap = [...keys].reduce((map, key) =>{
+    map[key.dataset.note]= key
+    return map
+}, {})
+
+
 keys.forEach(key => {
     key.addEventListener('click', () => playNote(key))
 })
+
+if (recordButton){
+    recordButton.addEventListener('click', togglerRecording)
+}
+
+if(saveButton){
+    saveButton.addEventListener('click', saveSong)
+}
+
+playButton.addEventListener('click', playSong)
+
 
 //Bind keyboard keys to computer keys
 document.addEventListener('keydown', e =>{
@@ -20,7 +45,43 @@ document.addEventListener('keydown', e =>{
     if (blackIndex > -1) playNote(blackKeys[blackIndex])
 })
 
+function togglerRecording(){
+    recordButton.classList.toggle('active')
+    if (isRecording()){
+        startRecording()
+    } else {
+        stopRecording()
+    }
+}
+
+function isRecording() {
+    return recordButton != null && recordButton.classList.contains('active')
+}
+
+function startRecording() {
+    recordingStartTime = Date.now()
+    songNotes = []
+    playButton.classList.remove('show')
+    saveButton.classList.remove('show')
+}
+
+function stopRecording() {
+    playSong()
+    playButton.classList.add('show')
+    saveButton.classList.add('show')
+}
+
+function playSong() {
+    if(songNotes.length == 0) return
+    songNotes.forEach(note =>{
+        setTimeout(() => {
+            playNote(keyMap[note.key])
+        }, note.startTime)
+    })
+}
+
 function playNote(key) {
+    if (isRecording()) recordNote(key.dataset.note)
     const noteAudio = document.getElementById(key.dataset.note)
     noteAudio.currentTime=0
     noteAudio.play()
@@ -29,4 +90,19 @@ function playNote(key) {
         key.classList.remove('active')
     })
 
+}
+
+function recordNote(note) {
+    songNotes.push({
+        key: note,
+        startTime: Date.now() - recordingStartTime
+    })
+}
+
+function saveSong(){
+    axios.post('/songs', { songNotes: songNotes}).then(res => {
+        songLink.classList.add('show')
+        songLink.href= `/songs/${res.data._id}`
+        
+    })
 }
--- server.js
+++ server.js
@@ -1,11 +1,50 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const Song = require('./models/song.js')
 const app = express()
+const mongodb = require('mongodb');
+const http = require('http');
+const nconf = require('nconf');
+nconf.argv().env().file('keys.json');
+
+const user = nconf.get('mongoUser');
+const pass = nconf.get('mongoPass');
+const host = nconf.get('mongoHost');
+
+let uri = `mongodb+srv://${user}:${pass}@${host}`;
+
+mongoose.connect(uri, {
+    useNewUrlParser: true, useUnifiedTopology: true
+})
+
+
 
 app.set('view engine', 'ejs')
+app.use(express.json())
 app.use(express.static('public'))
 
 app.get('/', (req, res)=>{
     res.render('index')
+})
+
+app.post('/songs', async (req, res)=> {
+    const song = new Song({
+        notes: req.body.songNotes
+    })
+
+    await song.save()
+
+    res.json(song)
+})
+
+app.get('/songs/:id', async(req, res) => {
+    let song
+    try {
+        song = await Song.findById(req.params.id)
+     } catch (e){
+         song = undefined
+     }  
+     res.render('index', {song: song})     
 })
 
 app.listen(5000)
