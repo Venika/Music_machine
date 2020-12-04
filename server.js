f9d39617fac44898ee4e3cdeeaa0a511671a2e30
Venika Gaur <Venika.gaur2610@gmail.com>
2020-12-04 13:29:27 -0600

    Heroku

--- server.js
+++ server.js
@@ -47,5 +47,5 @@ app.get('/songs/:id', async(req, res) => {
      res.render('index', {song: song})     
 })
 
-var port_number = server.listen(process.env.PORT || 3000);
-app.listen(port_number);
+const port = process.env.PORT || 3000;
+app.listen(port);
